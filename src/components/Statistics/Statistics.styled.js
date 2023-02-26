7beae29fa857c6bc1bc8430b23d8cd5d17493e99
Hanna Franko <107546169+W-Sei@users.noreply.github.com>
2023-02-26 04:42:33 +0200

    add shadows

--- src/components/Statistics/Statistics.styled.js
+++ src/components/Statistics/Statistics.styled.js
@@ -8,6 +8,9 @@ export const StyledStatistics = styled.div`
 export const StyledStatisticsText = styled.p`
   background: yellowgreen;
   border-radius: 10px;
+  box-shadow: rgba(240, 46, 170, 0.4) 5px 5px, rgba(240, 46, 170, 0.3) 10px 10px,
+    rgba(240, 46, 170, 0.2) 15px 15px, rgba(240, 46, 170, 0.1) 20px 20px,
+    rgba(240, 46, 170, 0.05) 25px 25px;
   padding: 10px;
   line-height: 30px;
   min-width: 80px;
@@ -16,6 +19,7 @@ export const StyledStatisticsText = styled.p`
   text-align: center;
   color: black;
 `;
+
 export const StyledStatisticsResult = styled.span`
   color: black;
   font-weight: 500;
